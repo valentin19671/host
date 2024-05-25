bac9acaa178b20700b549d9cb6fddc945f4efefd
valentin19671 <150732112+valentin19671@users.noreply.github.com>
2024-05-25 14:21:59 +0300

    1

---
+++ main.js
@@ -0,0 +1,68 @@
+/*function changeCss () {
+    let navElement = document.querySelector(".left-nav-pc");
+if (this.scrollY > 150){
+    navElement.style.top = '20px'
+    navElement.style.maxHeigh = '90vh'
+}
+  }
+
+  window.addEventListener("scroll", changeCss , false);
+*/
+
+
+/*let navBlockArr=document.querySelectorAll('.left-nav-block');
+
+let p1HoverArr=document.querySelectorAll('.p1-hover');
+
+navBlockArr[0].addEventListener('mouseover',()=>{
+    p1HoverArr[0].style.opacity='1';
+})
+
+navBlockArr[0].addEventListener('mouseout',()=>{
+    p1HoverArr[0].style.opacity='0';
+})
+
+
+navBlockArr[1].addEventListener('mouseover',()=>{
+    p1HoverArr[1].style.opacity='1';
+})
+
+navBlockArr[1].addEventListener('mouseout',()=>{
+    p1HoverArr[1].style.opacity='0';
+})
+
+
+navBlockArr[2].addEventListener('mouseover',()=>{
+    p1HoverArr[2].style.opacity='1';
+})
+
+navBlockArr[2].addEventListener('mouseout',()=>{
+    p1HoverArr[2].style.opacity='0';
+})
+
+
+navBlockArr[3].addEventListener('mouseover',()=>{
+    p1HoverArr[3].style.opacity='1';
+})
+
+navBlockArr[3].addEventListener('mouseout',()=>{
+    p1HoverArr[3].style.opacity='0';
+})
+
+
+navBlockArr[4].addEventListener('mouseover',()=>{
+    p1HoverArr[4].style.opacity='1';
+})
+
+navBlockArr[4].addEventListener('mouseout',()=>{
+    p1HoverArr[4].style.opacity='0';
+})
+
+
+navBlockArr[5].addEventListener('mouseover',()=>{
+    p1HoverArr[5].style.opacity='1';
+})
+
+navBlockArr[5].addEventListener('mouseout',()=>{
+    p1HoverArr[5].style.opacity='0';
+})*/
